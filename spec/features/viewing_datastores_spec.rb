bbfe34d5732c4265a05c0ec23e744c71c1b20043
Johnny Graber <jg@jgraber.ch>
2014-04-21 11:04:59 +0200

    improve layout for datastores (index)

--- spec/features/viewing_datastores_spec.rb
+++ spec/features/viewing_datastores_spec.rb
@@ -14,4 +14,15 @@ feature "Viewing datastores" do
     click_link 'DVD 1'
     expect(page.current_url).to eql(datastore_url(datastore))
   end
+
+  scenario "Show datastores as table" do
+    ds1 = FactoryGirl.create(:datastore, name: "Disk 1b", mediatype: "DVD")    
+    ds2 = FactoryGirl.create(:datastore, name: "Disk 2", mediatype: "CD")
+
+    visit '/'
+    find('tr', text: ds1.name).should have_content("DVD")
+
+    find('tr', text: ds2.id).should have_content("CD")
+    
+  end
 end
