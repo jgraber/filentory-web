6df6bd4a20fc0e4e54671dbe09d6f330336f19a3
Johnny Graber <jg@jgraber.ch>
2015-09-09 21:41:40 +0200

    add first idea for statistics overview

--- spec/features/viewing_overview_spec.rb
+++ spec/features/viewing_overview_spec.rb
@@ -4,6 +4,10 @@ feature "Viewing the overview" do
   let!(:user){ FactoryGirl.create(:user)}
 
   before do
+    10.times do |i|
+      FactoryGirl.create(:datafile, name: "file#{i}.txt", size: i * 10)
+    end
+
     sign_in_as!(user)
     visit '/'
   end
@@ -11,5 +15,9 @@ feature "Viewing the overview" do
 
   scenario "The overview welcomes the user" do
     expect(page).to have_content("Welcome to Filentory")
+  end
+
+  scenario "The number of files is shown" do
+    expect(page).to have_content("#Datafiles: 10")
   end
 end
