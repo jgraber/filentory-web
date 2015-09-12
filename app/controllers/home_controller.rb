20b0dfe34a972ba98299faf8abc4c86b39239921
Johnny Graber <jg@jgraber.ch>
2015-09-12 10:15:23 +0200

    move datafiles statistic part to own method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,20 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  	@statistics = Statistics.new
-  	@statistics.count = Datafile.count  	
-  	@statistics.total = Datafile.sum(:size)
-  	@statistics.min = Datafile.minimum(:size)
-  	@statistics.max = Datafile.maximum(:size)
-  	@statistics.average = Datafile.average(:size)
+  	@statistics = collect_datafile_statistics
 
   	@datastore_types = Datastore.group(:mediatype).count.sort_by {|k, v| v}.reverse.to_h
+  end
+
+  private
+  def collect_datafile_statistics
+  	statistics = Statistics.new
+  	statistics.count = Datafile.count  	
+  	statistics.total = Datafile.sum(:size)
+  	statistics.min = Datafile.minimum(:size)
+  	statistics.max = Datafile.maximum(:size)
+  	statistics.average = Datafile.average(:size)
+  	
+  	statistics
   end
 end
