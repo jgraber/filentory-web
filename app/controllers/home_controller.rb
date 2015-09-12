c47031600dae8e20f0331874b88fd21963e26a40
Johnny Graber <jg@jgraber.ch>
2015-09-12 08:39:47 +0200

    Show total size of all files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
   def index
   	@statistics = Statistics.new
-  	@statistics.count = Datafile.count
+  	@statistics.count = Datafile.count  	
+  	@statistics.total = Datafile.sum(:size)
   end
 end
--- spec/features/viewing_overview_spec.rb
+++ spec/features/viewing_overview_spec.rb
@@ -20,4 +20,10 @@ feature "Viewing the overview" do
   scenario "The number of files is shown" do
     expect(page).to have_content("#Datafiles: 10")
   end
+
+  scenario "The size of all files is shown" do
+    size = 0*10 + 1*10 + 2*10 + 3*10 + 4*10 +5*10 + 6*10 + 7*10 + 8*10 + 9*10
+    expect(page).to have_content("Total size: #{size}")
+  end
+
 end
