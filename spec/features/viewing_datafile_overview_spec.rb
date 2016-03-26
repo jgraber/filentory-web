1d8ae420e83f764746f0dd5b9a1e02d6dadeb6ed
Johnny Graber <jg@jgraber.ch>
2016-03-26 22:02:33 +0100

    use human readable units for statistic display

--- spec/features/viewing_datafile_overview_spec.rb
+++ spec/features/viewing_datafile_overview_spec.rb
@@ -25,24 +25,24 @@ feature "Viewing the Datafile overview" do
   scenario "The size of all files is shown" do
     size = 0*10 + 1*10 + 2*10 + 3*10 + 4*10 +5*10 + 6*10 + 7*10 + 8*10 + 9*10
     expect(page).to have_selector("table tbody tr:nth-of-type(2) td:nth-of-type(1)", text: 'Total size:') # second row, first column
-    expect(page).to have_selector("table tbody tr:nth-of-type(2) td:nth-of-type(2)", text: size) # second row, second column
+    expect(page).to have_selector("table tbody tr:nth-of-type(2) td:nth-of-type(2)", text: "#{size} Bytes") # second row, second column
   end
 
   scenario "The minimum size of all files is shown" do
     minimumSize = 0*10
     expect(page).to have_selector("table tbody tr:nth-of-type(3) td:nth-of-type(1)", text: 'Minimum size:') # third row, first column
-    expect(page).to have_selector("table tbody tr:nth-of-type(3) td:nth-of-type(2)", text: minimumSize) # third row, second column
+    expect(page).to have_selector("table tbody tr:nth-of-type(3) td:nth-of-type(2)", text: "#{minimumSize} Bytes") # third row, second column
   end
 
   scenario "The maximum size of all files is shown" do
     maximumSize = 9*10
     expect(page).to have_selector("table tbody tr:nth-of-type(4) td:nth-of-type(1)", text: 'Maximum size:') # forth row, first column
-    expect(page).to have_selector("table tbody tr:nth-of-type(4) td:nth-of-type(2)", text: maximumSize) # forth row, second column
+    expect(page).to have_selector("table tbody tr:nth-of-type(4) td:nth-of-type(2)", text: "#{maximumSize} Bytes") # forth row, second column
   end
 
   scenario "The average size of all files is shown" do
     averageSize = (0*10 + 1*10 + 2*10 + 3*10 + 4*10 +5*10 + 6*10 + 7*10 + 8*10 + 9*10) / 10
     expect(page).to have_selector("table tbody tr:nth-of-type(5) td:nth-of-type(1)", text: 'Average size:') # fifth row, first column
-    expect(page).to have_selector("table tbody tr:nth-of-type(5) td:nth-of-type(2)", text: averageSize) # fifth row, second column
+    expect(page).to have_selector("table tbody tr:nth-of-type(5) td:nth-of-type(2)", text: "#{averageSize} Bytes") # fifth row, second column
   end
 end
