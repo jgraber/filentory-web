92b5bdccef94812fb9f69c6dca70f607ff7db340
Johnny Graber <jg@jgraber.ch>
2020-05-18 23:00:55 +0200

    fix Devise warning

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -9,7 +9,7 @@ feature 'Signing up' do
   
   scenario 'Successful sign up' do
     fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
+    fill_in "user[password]", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
   
@@ -18,10 +18,10 @@ feature 'Signing up' do
 
   scenario 'wrong passwort confirmation fails sign up' do
     fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
+    fill_in "user[password]", with: "password"
     fill_in "Password confirmation", with: "password2"
     click_button "Sign up"
   
-    expect(page).to have_content("Password confirmation doesn't match Password")
+    expect(page).to have_content("doesn't match Password")
   end
 end
